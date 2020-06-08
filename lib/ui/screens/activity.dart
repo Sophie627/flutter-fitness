a11d2d1b6f2f21067d6180772b6b7d5e583e4de1
Sophie <alekseizukovski@gmail.com>
2020-06-08 06:32:18 +0300

    update activity screen

--- lib/ui/screens/activity.dart
+++ lib/ui/screens/activity.dart
@@ -14,261 +14,182 @@ class ActivityScreen extends StatefulWidget {
 
 class _ActivityScreenState extends State<ActivityScreen> {
 CalendarController _calendarController= CalendarController();
-@override
-void initState() {
-  super.initState();
-  _calendarController = CalendarController();
-}
+  
+  @override
+  void initState() {
+    super.initState();
+    _calendarController = CalendarController();
+  }
 
-@override
-void dispose() {
-  _calendarController.dispose();
-  super.dispose();
-}
+  @override
+  void dispose() {
+    _calendarController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-          child: Scaffold(
-        backgroundColor: Colors.grey.shade400,
-        appBar: AppBar(
-          elevation: 0,
+    return Container(
+      color: Colors.grey.shade400,
+      child: SafeArea(
+        child: Scaffold(
           backgroundColor: Colors.grey.shade400,
-          centerTitle: true,
-          leading: new IconButton(
-            icon: new Icon(Icons.arrow_back),
-            onPressed: () => Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => MainScreen(
-                  settings: widget.settings,
-                )),
-            )),
-          title: Text(
-            'Activity',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.black,
-            ),
-          ),
-        ),
-        body: Column(
-            
-            children: <Widget>[
-          Expanded(
-          child: Row(
-          mainAxisSize: MainAxisSize.min,
-            children: [
-        Expanded(
-          child: InkWell(
-            onTap: () {
-              Navigator.push(
+          appBar: AppBar(
+            elevation: 0,
+            backgroundColor: Colors.grey.shade400,
+            centerTitle: true,
+            leading: new IconButton(
+              icon: new Icon(Icons.arrow_back),
+              onPressed: () => Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => TotalWorkouts(
+                  builder: (context) => MainScreen(
                     settings: widget.settings,
                   )),
-              );
-            },
-            child: ReUseableCard(
-              cardChild: Column(children: [
-                SizedBox(height: 25),
-                Text(
-                  '7',
-                  style: TextStyle(
-                    fontSize: 35,
-                    color: Colors.black,
-                  ),
-                ),
-                Text(
-                  'TOTAL WORKOUTS COMPLETED',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black,
-                  ),
-                ),
-                Expanded(
-                  child: Container(
-                    margin: EdgeInsets.only(top: 10.0),
-                    decoration: BoxDecoration(
-                        color: Colors.greenAccent,
-                        borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(10),
-                          bottomRight: Radius.circular(10),
-                        )),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Text(
-                        'VIEW ALL',
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                    ),
-                    width: double.infinity,
-                    height: 15,
-                  ),
-                ),
-              ]),
-            ),
-          ),
-          
-        ),
-        Expanded(
-          child: ReUseableCard(
-            cardChild: Column(children: [
-                SizedBox(height: 25),
-              Text(
-                '0',
-                style: TextStyle(
-                  fontSize: 35,
-                  color: Colors.black,
-                ),
-              ),
-              Text(
-                'TOTAL CHALLENGES COMPLETED',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 12,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  margin: EdgeInsets.only(top: 10.0),
-                  decoration: BoxDecoration(
-                      color: Colors.greenAccent,
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(10),
-                        bottomRight: Radius.circular(10),
-                      )),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'VIEW ALL',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontSize: 12,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                  width: double.infinity,
-                  height: 15,
-                ),
-              ),
-            ]),
-          ),
-        ),
-              ])),
-              Expanded(
-          child: ReUseableCard(
-        cardChild: Column(
-          children: <Widget>[
-            SizedBox(height: 10, width: double.infinity),
-            Text(
-              '0',
+              )),
+            title: Text(
+              'Activity',
               style: TextStyle(
-                fontSize: 25,
-                color: Colors.black,
-              ),
-            ),
-            Expanded(
-                              child: Row(
-                                mainAxisSize: MainAxisSize.min,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Expanded(
-                child: ReUseTile(
-                  headerText: '0',
-                  footerText: 'S',
-                  iconColor: Colors.grey,
-                ),
-              ),
-              Expanded(
-              child: ReUseTile(
-                headerText: '0',
-                footerText: 'M',
-                iconColor: Colors.greenAccent,
-              ),
-
-            ),
-            Expanded(
-              child: ReUseTile(
-                headerText: '0',
-                footerText: 'T',
-                iconColor: Colors.grey,
-              ),
-            ),
-            Expanded(
-              child: ReUseTile(
-                headerText: '0',
-                footerText: 'W',
-                iconColor: Colors.grey,
-              ),
-            ),
-            Expanded(
-              child: ReUseTile(
-                headerText: '0',
-                footerText: 'T',
-                iconColor: Colors.grey,
-              ),
-            ),
-            Expanded(
-              child: ReUseTile(
-                headerText: '0',
-                footerText: 'F',
-                iconColor: Colors.grey,
-              ),
-            ),
-            Expanded(
-              child: ReUseTile(
-                headerText: '0',
-                footerText: 'S',
-                iconColor: Colors.grey,
-              ),
-            ),
-                ],
-              ),
-            ),
-            Text(
-              'CALORIES BURNED',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 12,
                 fontWeight: FontWeight.bold,
                 color: Colors.black,
               ),
             ),
-          ],
-        ),
-              )),
+          ),
+          body: Column(
+            children: <Widget>[
               Expanded(
-                            child: ReUseableCard(
-        cardChild: TableCalendar(
-      calendarController: _calendarController,
-headerVisible: true,
-locale: 'en_US',
-initialCalendarFormat: CalendarFormat.week,
-headerStyle: HeaderStyle(
-        centerHeaderTitle: true,
-        formatButtonVisible: false,
-      ),
-
-  ),
+                flex: 1,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Expanded(
+                      child: InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => TotalWorkouts(
+                                settings: widget.settings,
+                              )),
+                          );
+                        },
+                        child: ReUseableCard(
+                          cardChild: Column(children: [
+                            SizedBox(height: 25),
+                            Text(
+                              '7',
+                              style: TextStyle(
+                                fontSize: 35,
+                                color: Colors.black,
+                              ),
+                            ),
+                            Text(
+                              'TOTAL WORKOUTS COMPLETED',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                            Expanded(
+                              child: Container(
+                                margin: EdgeInsets.only(top: 10.0),
+                                decoration: BoxDecoration(
+                                    color: Colors.greenAccent,
+                                    borderRadius: BorderRadius.only(
+                                      bottomLeft: Radius.circular(10),
+                                      bottomRight: Radius.circular(10),
+                                    )),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    'VIEW ALL',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                                width: double.infinity,
+                                height: 15,
+                              ),
+                            ),
+                          ]),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: ReUseableCard(
+                        cardChild: Column(children: [
+                            SizedBox(height: 25),
+                          Text(
+                            '0',
+                            style: TextStyle(
+                              fontSize: 35,
+                              color: Colors.black,
+                            ),
+                          ),
+                          Text(
+                            'TOTAL CHALLENGES COMPLETED',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black,
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              margin: EdgeInsets.only(top: 10.0),
+                              decoration: BoxDecoration(
+                                  color: Colors.greenAccent,
+                                  borderRadius: BorderRadius.only(
+                                    bottomLeft: Radius.circular(10),
+                                    bottomRight: Radius.circular(10),
+                                  )),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  'VIEW ALL',
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                              width: double.infinity,
+                              height: 15,
+                            ),
+                          ),
+                        ]),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-              
+              Expanded(
+                flex: 3,
+                child: ReUseableCard(
+                  cardChild: TableCalendar(
+                    calendarController: _calendarController,
+                    headerVisible: true,
+                    locale: 'en_US',
+                    initialCalendarFormat: CalendarFormat.month,
+                    headerStyle: HeaderStyle(
+                      centerHeaderTitle: true,
+                      formatButtonVisible: false,
+                    ),
+                  ),
+                ),
+              ),
             ],
-            
           ),
+        ),
       ),
     );
   }
